b55bb15468475a7f4ebe5ab76ec5a23d126d1792
realfolkcode <wtfomgrofl@mail.ru>
2021-12-05 19:25:11 +0300

    Update commands.sh and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN pip install -U scikit-learn
 RUN pip install tqdm
 
 RUN chmod +x scipts/
+WORKDIR scripts
 
 ENTRYPOINT ["/scripts/commands.sh"]
 
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./download.sh pdbsmall
-python ../train.py --cuda -1 --model_dir sign/models --dataset pdbsmall --cut_dist 5 --num_angle 6
+python ../train.py --cuda -1 --model_dir sign/models --dataset pdbsmall --cut_dist 5 --num_angle 6 > ../logs/train_log
