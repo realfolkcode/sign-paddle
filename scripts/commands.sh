d812f1090b025a79635c766017098db1d4708d13
realfolkcode <wtfomgrofl@mail.ru>
2021-12-05 23:28:17 +0300

    Fix paths

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$PARENT_PATH"
+
 ./download.sh pdbsmall
-python train.py --cuda -1 --model_dir ./models --dataset pdbsmall --cut_dist 5 --num_angle 6 > ./logs/train_log
+python ../train.py --cuda -1 --model_dir ../models --dataset pdbsmall --cut_dist 5 --num_angle 6 > ../logs/train_log
