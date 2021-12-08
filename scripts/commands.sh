6fa570e565aaefa10d94e015ebe0a8494fb9b00c
realfolkcode <wtfomgrofl@mail.ru>
2021-12-08 14:53:17 +0300

    Change base image and change dataset to small

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,5 +4,5 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$PARENT_PATH"
 
-./download.sh pdbbind2016
+./download.sh pdbsmall
 python ../train.py --cuda -1 --model_dir ../models --data_dir ../data --dataset pdbsmall --cut_dist 5 --num_angle 6 > ../logs/train_log
