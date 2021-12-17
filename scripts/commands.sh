637a58c22d58dc222d371c49370bc61291ae7235
realfolkcode <wtfomgrofl@mail.ru>
2021-12-17 17:24:31 +0300

    Fix commands.sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -4,5 +4,5 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$PARENT_PATH"
 
-./download.sh pdbbind2016
+./download.sh pdb100
 python ../train.py --cuda 0 --model_dir ../models --data_dir ../data --dataset pdb100 --cut_dist 5 --num_angle 6
