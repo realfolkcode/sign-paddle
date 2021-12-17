2db3439f0ef6feb64669cc2aec28c33302a3c2f6
realfolkcode <wtfomgrofl@mail.ru>
2021-12-17 11:45:48 +0300

    Add dataset pdb100

--- scripts/download.sh
+++ scripts/download.sh
@@ -14,6 +14,10 @@ elif [ "$DATASET" == "pdbbind2016" ]; then
 	TRAIN_URL="https://www.dropbox.com/sh/68vc7j5cvqo4p39/AAB9fWY5sYoiTmEzHU4iXFPfa/pdbbind2016_train.pkl"
 	VAL_URL="https://www.dropbox.com/sh/68vc7j5cvqo4p39/AAB5RuvgkQI1q4Hnl-C0J6mOa/pdbbind2016_val.pkl"
 	TEST_URL="https://www.dropbox.com/sh/68vc7j5cvqo4p39/AAAJkCL1mrSyi6HqwhdW79bNa/pdbbind2016_test.pkl"
+elif [ "$DATASET" == "pdb100" ]; then
+	TRAIN_URL="https://www.dropbox.com/s/k8i3ut2oj6nvdzq/pdb100_train.pkl"
+	VAL_URL="https://www.dropbox.com/s/84lpa1o6svj92uc/pdb100_val.pkl"
+	TEST_URL="https://www.dropbox.com/s/wktpexd5iuk52ck/pdb100_test.pkl"
 fi
 
 wget -P "../data" $TRAIN_URL
