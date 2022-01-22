95e3ce3be534b6cc089791ec481d198927f3c582
realfolkcode <wtfomgrofl@mail.ru>
2022-01-22 18:18:12 +0300

    Fix dataset sizes

--- train.py
+++ train.py
@@ -160,8 +160,8 @@ if __name__ == "__main__":
     else:
         paddle.set_device('gpu:%s' % args.cuda)
     trn_complex = ComplexDataset(args.data_dir, "%s_train" % args.dataset, args.cut_dist, args.num_angle, 0, 3389)
-    tst_complex = ComplexDataset(args.data_dir, "%s_test" % args.dataset, args.cut_dist, args.num_angle, 0, 376)
-    val_complex = ComplexDataset(args.data_dir, "%s_val" % args.dataset, args.cut_dist, args.num_angle, 289)
+    tst_complex = ComplexDataset(args.data_dir, "%s_test" % args.dataset, args.cut_dist, args.num_angle, 0, 289)
+    val_complex = ComplexDataset(args.data_dir, "%s_val" % args.dataset, args.cut_dist, args.num_angle, 376)
     trn_loader = Dataloader(trn_complex, args.batch_size, shuffle=True, num_workers=args.num_workers, collate_fn=collate_fn)
     tst_loader = Dataloader(tst_complex, args.batch_size, shuffle=False, num_workers=1, collate_fn=collate_fn)
     val_loader = Dataloader(val_complex, args.batch_size, shuffle=False, num_workers=1, collate_fn=collate_fn)
